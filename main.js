319c8eef5b13df2310a055ac933cc75caf225995
kdhiltb <69743882+kdhiltb@users.noreply.github.com>
2024-06-10 09:57:21 -0400

    Create calculation variables

--- main.js
+++ main.js
@@ -17,3 +17,13 @@ console.log(ops.add(1,2));
 console.log(ops.subtract(1,2));
 console.log(ops.multiply(1,2));
 console.log(ops.divide(1,2));
+
+let first = 0; // first number
+let second = 0; // second number
+let operator = ""; // operator
+
+function operate(first, second, operator) {
+    return ops[operator](first, second);
+}
+
+console.log(operate(1, 2, "divide"));
