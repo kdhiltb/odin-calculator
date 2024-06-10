84a2c9cdc81b652a2692100ce0ef6d06661b5aae
kdhiltb <69743882+kdhiltb@users.noreply.github.com>
2024-06-10 09:45:23 -0400

    Create math functions

---
+++ main.js
@@ -0,0 +1,19 @@
+ops = {
+    add: function(a, b) {
+        return a + b;
+    },
+    subtract: function(a, b) {
+        return a - b;
+    },
+    multiply: function(a, b) {
+        return a * b;
+    },
+    divide: function(a, b) {
+        return a / b;
+    }
+}
+
+console.log(ops.add(1,2));
+console.log(ops.subtract(1,2));
+console.log(ops.multiply(1,2));
+console.log(ops.divide(1,2));
