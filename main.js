ab52c8a50d6d4940c2013e692691874681a92010
kdhiltb <69743882+kdhiltb@users.noreply.github.com>
2024-06-11 11:27:00 -0400

    Add math functions and calculator operations

--- main.js
+++ main.js
@@ -1,43 +1,97 @@
-ops = {
-    add: function(a, b) {
-        return a + b;
-    },
-    subtract: function(a, b) {
-        return a - b;
-    },
-    multiply: function(a, b) {
-        return a * b;
-    },
-    divide: function(a, b) {
-        return a / b;
-    }
-}
+let ops = {}
+ops["+"] = function(a, b) {return a + b};
+ops["-"] = function(a, b) {return a - b};
+ops["*"] = function(a, b) {return a * b};
+ops["/"] = function(a, b) {return a / b};
 
-console.log(ops.add(1,2));
-console.log(ops.subtract(1,2));
-console.log(ops.multiply(1,2));
-console.log(ops.divide(1,2));
-
-let first = 0; // first number
-let second = 0; // second number
-let operator = ""; // operator
+let displayVal = {};
+displayVal.first = 0;
+displayVal.second = null;
+displayVal.operator = null;
 
 function operate(first, second, operator) {
     return ops[operator](first, second);
 }
 
-console.log(operate(1, 2, "divide"));
-
-
-const nums = document.querySelector(".nums");
-for (let i = 0; i < 9; i++) {
-    const btn = document.createElement("div");
-    const num = document.createElement("p");
-    btn.classList.add("btn");
-    btn.classList.add("center");
-    nums.appendChild(btn);
-    btn.appendChild(num);
-    if (i < 3) num.textContent = i + 7;
-    if (i < 6 && i >= 3) num.textContent = i + 1;
-    if (i < 9 && i >= 6) num.textContent = i - 5;
+function createNums() {
+    const nums = document.querySelector(".nums");
+    for (let i = 0; i < 9; i++) {
+        const btn = document.createElement("div");
+        const num = document.createElement("p");
+        btn.classList.add("btn");
+        btn.classList.add("center");
+        btn.classList.add("num");
+        nums.appendChild(btn);
+        btn.appendChild(num);
+        if (i < 3) num.textContent = i + 7;
+        if (i < 6 && i >= 3) num.textContent = i + 1;
+        if (i < 9 && i >= 6) num.textContent = i - 5;
+    }
 }
+
+function displayNewNum(btnClick) {
+    num = Number(btnClick);
+    display.textContent = btnClick;
+    return num;
+}
+
+function updateDisplay(btnClick, num, eval) {
+    if (eval.type === "num") {
+        if (!eval.status) {
+            if (!num) {
+                num = displayNewNum(btnClick);
+            } else {
+                num = Number(num.toString() + btnClick);
+                display.textContent = num;
+            }
+            return num;
+        } else {
+            num = displayNewNum(btnClick);
+            eval.status = false;
+            return num;
+        }
+    } else {
+        display.textContent = num;
+    }
+
+}
+
+createNums();
+
+const btns = document.querySelectorAll(".btn");
+const display = document.querySelector(".display");
+let eval = {};
+btns.forEach((btn) => btn.addEventListener("click", function (e) {
+    let btnClick = e.target.textContent;
+    if (btn.matches(".num")) {
+        eval.type = "num";
+        if (displayVal.operator === null) {
+            displayVal.first = updateDisplay(btnClick, displayVal.first, eval);
+        } else {
+            displayVal.second = updateDisplay(btnClick, displayVal.second, eval);
+        }
+    } else if (btn.matches(".op")) {
+        displayVal.operator = btnClick;
+        eval.status = false;
+    } else if (btn.matches(".equals")) {
+        eval.status = true;
+        eval.type = "equals"
+        if (displayVal.operator) {
+            if (displayVal.second === null) {
+                displayVal.second = displayVal.first;
+            }
+            displayVal.first = operate(displayVal.first, displayVal.second, displayVal.operator);
+                console.log("Result: " + displayVal.first);
+                console.log('debug' + btnClick);
+                console.log('debug' + displayVal.first);
+                console.log('debug' + eval.type);
+                updateDisplay(btnClick, displayVal.first, eval.type);
+                displayVal.second = null;
+                displayVal.operator = null;
+        }
+    }
+    console.log("Button: " + btnClick);
+    console.log('First: ' + displayVal.first);
+    console.log('Second: ' + displayVal.second);
+    console.log('Op: ' + displayVal.operator);
+  }));
