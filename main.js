673e0229cfbaf0ae11a3ce3aa70faeab8c59dbfe
kdhiltb <69743882+kdhiltb@users.noreply.github.com>
2024-06-12 10:54:23 -0400

    Fix decimal place bugs

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ ops["/"] = function(a, b) {
 
 function resetDisplayVal() {
     displayVal.first = 0;
-    displayVal.second = null;
+    displayVal.second = 0;
     displayVal.operator = null;
 }
 
@@ -20,7 +20,7 @@ let displayVal = {};
 resetDisplayVal();
 
 function operate(first, second, operator) {
-    return ops[operator](first, second);
+    return Number(ops[operator](Number(first), Number(second)).toFixed(14));
 }
 
 function createNums() {
@@ -40,16 +40,18 @@ function createNums() {
 }
 
 function displayNewNum(btnClick) {
-
-    if (eval.decimal === true) {
+    if (eval.decimal.status === true) {
         if (!Number(btnClick)) {
-            btnClick = 0 + btnClick;
+            if(Number(btnClick) === 0 && btnClick !== "0.") {
+                btnClick = '0.' + btnClick;
+            }
         } else {
             btnClick = '0.' + btnClick;
         }
     }
-    display.textContent = btnClick;
-    num = Number(btnClick);
+    displayVal.numString = btnClick;
+    display.textContent = displayVal.numString;
+    num = btnClick;
     return num;
 }
 
@@ -61,23 +63,54 @@ function updateDisplay(btnClick, num, eval) {
     } else if (eval.type === "num") {
         if (!eval.status) {
             if (!num) {
-                num = displayNewNum(btnClick);
-            } else {
-                if (btnClick === ".") {
-                    console.log(num);
-                    num = num + btnClick;
+                console.log("ITS HERE");
+                if (eval.decimal.status === true) {
+                    console.log("NUM0?: " + num);
+                    console.log("LOG" + btnClick);
+                    console.log("LOG" + num);
+                    if (num === 0 && eval.decimal.initial) displayVal.numString = 0;
+                    eval.decimal.initial = false;
+                    displayVal.numString = displayVal.numString + btnClick;
+                    console.log("HEY: " + displayVal.numString);
+                    num = displayVal.numString;
+                    display.textContent = num;
                 } else {
-                    if (eval.decimal) {
-                        num = Number(num + (btnClick / 10))
+                    num = displayNewNum(btnClick)
+                }
+            } else {
+                if (btnClick === "." && eval.decimal.initial) {
+                    num = num + btnClick;
+                } else if (btnClick === ".") {
+                    return;
+                } else {
+                    if (eval.decimal.status && eval.decimal.initial) {
+                        console.log("CHECKING: " + num);
+                        decNum = btnClick / 10;
+                        if (decNum !== 0) {
+                            num = num + "." + decNum.toString().substring(2,decNum.length);
+                        } else {
+                            num = num + "." + decNum.toString();
+                        }
+                        eval.decimal.status = false;
+                        eval.decimal.initial = false;
+                        console.log("GOT IT");
                     } else {
-                        num = Number(num.toString() + btnClick);
+                        num = num.toString() + btnClick;
                     }
                 }
                 display.textContent = num;
+                console.log("NEW NUM IS: " + num);
+                console.log(eval.decimal.status);
             }
             return num;
         } else {
-            num = displayNewNum(btnClick);
+            console.log("WHA TIS NUM: " + btnClick);
+            console.log("EVAL: " + eval.decimal.status);
+            if (btnClick === ".") {
+                num = displayNewNum("0.");
+            } else {
+                num = displayNewNum(btnClick);
+            }
             eval.status = false;
             return num;
         }
@@ -94,6 +127,9 @@ const btns = document.querySelectorAll(".btn");
 const display = document.querySelector(".display");
 let eval = {};
 let errMessage;
+eval.decimal = {}
+eval.decimal.status = false;
+eval.decimal.initial = true;
 btns.forEach((btn) => btn.addEventListener("click", function (e) {
     let btnClick = e.target.textContent;
     let valToUpdate;
@@ -113,12 +149,15 @@ btns.forEach((btn) => btn.addEventListener("click", function (e) {
         if (!displayVal.second) {
             displayVal.operator = btnClick;
             eval.status = false;
-            eval.decimal = false;
+            eval.decimal.status = false;
+            eval.decimal.initial = true;
+            displayVal.second = 0;
         }
     } else if (btn.matches(".equals")) {
         eval.status = true;
         eval.type = "equals"
-        eval.decimal = false;
+        eval.decimal.status = false;
+        eval.decimal.initial = true;
         if (displayVal.operator) {
             if (displayVal.second === null) {
                 displayVal.second = displayVal.first;
@@ -126,13 +165,15 @@ btns.forEach((btn) => btn.addEventListener("click", function (e) {
             displayVal.first = operate(displayVal.first, displayVal.second, displayVal.operator);
             if (displayVal.first === null) errMessage = "NOPE";
             updateDisplay(btnClick, displayVal.first, eval);
-            displayVal.second = null;
+            displayVal.second = 0;
             displayVal.operator = null;
         }
+        if (displayVal.first.toString().includes(".")) eval.decimal.present = true;
     } else if (btn.matches(".clear")) {
         eval.status = false;
         eval.type = null;
-        eval.decimale = false;
+        eval.decimal.status = false;
+        eval.decimal.initial = true;
         resetDisplayVal();
         updateDisplay(0, displayVal.first, eval);
     } else if (btn.matches(".negate") || btn.matches(".percent")) {
@@ -145,13 +186,14 @@ btns.forEach((btn) => btn.addEventListener("click", function (e) {
         displayVal[valToUpdate] = updateDisplay(btnClick, displayVal[valToUpdate] * mod, eval);
         eval.type = initEval;
     } else if (btn.matches(".decimal")) {
-        if (eval.decimal === true) return 0;
+        if (eval.decimal.status === true) return 0;
+        if (eval.status === true) displayVal.first = "0";
+        console.log(displayVal[valToUpdate] + "HI");
+        console.log(valToUpdate);
         eval.type = "num";
-        eval.decimal = true;
+        eval.decimal.status = true;
         updateDisplay(btnClick, displayVal[valToUpdate], eval);
+        eval.status = false;
+        eval.decimal.present = true;
     }
-    console.log("Button: " + btnClick);
-    console.log('First: ' + displayVal.first);
-    console.log('Second: ' + displayVal.second);
-    console.log('Op: ' + displayVal.operator);
   }));
